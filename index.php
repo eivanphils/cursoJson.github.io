7088977ce5c93a227d6fa513fc5a18ab22e6465e
Phils Garcia <eivanphils@gmail.com>
2015-03-15 18:22:59 -0430

    nuevos archivos para json de prueba

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <title>Json</title>
+</head>
+
+    <body>
+        <h1>Objeto json</h1>
+        <div id="contenedor"></div>
+        
+        <script type="text/javascript" src="json.js"></script>
+        
+        <script>alert(data.nombre);
+            alert(data.idProyectos[2]);
+            alert(data.cursos.proyectos[1]);</script>
+        
+    </body>
+</html>
---
+++ json.js
@@ -0,0 +1,13 @@
+var data = {
+    "nombre": "phils",
+    "id": 123,
+    "trabajador": true,
+    "idProyectos" : [142, 132, 122],
+    "cursos" : {
+        "proyectos": [
+            "cala",
+            "nos.3",
+            "magazine"]
+    },
+    "sexo": "masculino"
+};
